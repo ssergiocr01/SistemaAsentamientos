5436dba573220f459ed03c168da4cba1c3eb7f56
Sergio Serrano Vargas <ssergiocr01@gmail.com>
2018-11-04 10:29:00 -0600

    Inicio del Sistema

---
+++ SistemaAsentamientos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaAsentamientos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
